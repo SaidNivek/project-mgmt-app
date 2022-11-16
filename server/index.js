7d99ceaf1d9960330bf1ec104013f76acb44a87a
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-16 13:34:33 -0500

    Setup ClientType and RootQuery in schema, export the schema for app.use

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// ~~~~~ EXPRESS SETUP ~~~~~
 // Bringing in express for our server
 const express = require('express')
 // Needed for the .env files to be used properly
@@ -11,12 +12,16 @@ const port = process.env.PORT || 4000
 // Creating the app for things to actually start listening and happening with our code
 const app = express()
 
+// ~~~~~ ENDPOINTS ~~~~~
 // Define app.use so it knows the endpoint for our server
-app.use('/graphql', graphqlHTTP({
+app.use(
+    '/graphql',
+    graphqlHTTP({
     schema: schema,
     // Checks to see if we are in development and, if we are, sets graphiql to true, so we can use it
     graphiql: process.env.NODE_ENV === 'development'
 }))
 
+// ~~~~~ LISTEN ~~~~~
 // Checking to see if the server is up and running properly
 app.listen(port, console.log(`Server running on port ${port}.`))
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,42 @@
 // Bring in the sampleData and destructure it for us to work with before the DB is set up in MongoDB
 const { projects, clients } = require('../sampleData.js')
+
+// Bring in the things from graphql that we need to use and destructure them into their parts
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql')
+
+// Client Type - GraphQL Object Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    // This fields for the ClientType is an anonymous function that returns an object
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    // This fields for the RootQuery is an object that returns queries
+    fields: {
+        // This defines the client query
+        client: {
+            // It takes in a type, the client type, which we created above
+            type: ClientType,
+            // To get a specific client, we need to pass in args to specify which one
+            // id is the GraphQLID that we set up in the ClientType, above
+            args: { id: {type: GraphQLID}},
+            // Our return goes into this resolver, which is a function that takes in a parent and args
+            resolve(parent, args) {
+                // Uses .find on the clients array, then matches the array from args, which is passed in, to the id of the client that exists within the array
+                return clients.find(client => client.id === args.id)
+            }
+        }
+    }
+})
+
+// In order to use the query, we need to export the schema
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
