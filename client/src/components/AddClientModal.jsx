c040d210950ce6ac611ead909d1b59fc65206837
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-23 15:05:57 -0500

    Modal values show proper inputs, not connected to DB

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -3,28 +3,48 @@ import { FaUser } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
 
 export default function AddClientModal() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+
   return (
     <>
         {/* <!-- Button trigger modal --> */}
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Add Client
+        <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
+            <div className="d-flex align-items-center">
+                <FaUser className="icon" />
+                <div>Add Client</div>
+            </div>
         </button>
 
         {/* <!-- Modal --> */}
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id="addClientModal" tabIndex="-1" aria-labelledby="addClientModalLabel" aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
-            <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-                ...
-            </div>
-            <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="addClientModalLabel">Add Client</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <form>
+                        <div className="mb-3">
+                            <label className="form-label">Name</label>
+                            <input type="text" className="form-control" id="name" value={name} 
+                            onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Email</label>
+                            <input type="email" className="form-control" id="email" value={email} 
+                            onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Phone</label>
+                            <input type="text" className="form-control" id="phone" value={phone} 
+                            onChange={(e) => setPhone(e.target.value)} />
+                        </div>
+                    </form>
+                </div>            
             </div>
         </div>
         </div>
