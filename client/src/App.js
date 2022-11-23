33bd6e27ad8601adab3b02c0bfd6cedf1cbb46ff
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-23 12:36:00 -0500

    Handle the warning when updating cache (existing vs incoming merge threw error

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,30 @@ import Header from './components/Header'
 import Clients from './components/Clients';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
+// This code below will prevent the merge error from displaying in the console and handles the existing and incoming query merges when the mutations call for an update(cache) function
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+      }
+    }
+  }
+})
+
 // This will create the Apollo client, which will wrap the app so we can access all of the graphql data we need in all of the parts of the app that needs it
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
