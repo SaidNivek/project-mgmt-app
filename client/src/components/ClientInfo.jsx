cc108e14f0f3cf47292cd1ec09cbcf73de3b75d6
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-04 16:34:23 -0500

    Connect ClientInfo.jsx to Project page

---
+++ client/src/components/ClientInfo.jsx
@@ -0,0 +1,7 @@
+
+
+export default function ClientInfo({client}) {
+  return (
+    <div>ClientInfo</div>
+  )
+}
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import { useQuery } from '@apollo/client'
 import { GET_PROJECT } from '../queries/projectQueries'
+import ClientInfo from '../components/ClientInfo'
 
 export default function Project() {
   // destructure the id of the project with useParams
@@ -13,8 +14,21 @@ export default function Project() {
 
   if (loading) return <Spinner />
   if (error) return <p>Something went wrong</p>
-  
+
   return (
-    <div>Project</div>
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">Back</Link>
+
+          <h1>{ data.project.name }</h1>
+          <p>{ data.project.description }</p>
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{ data.project.status }</p>
+
+          <ClientInfo client={ data.project.client } />
+        </div>
+      )}
+    </>
   )
 }
