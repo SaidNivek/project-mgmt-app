6fc7c72ad51f75bffd6752bfd70b27e5ebbaaeb6
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-04 16:26:15 -0500

    single project loading and error complete

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,19 @@
-
+import { Link, useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+import { useQuery } from '@apollo/client'
+import { GET_PROJECT } from '../queries/projectQueries'
 
 export default function Project() {
+  // destructure the id of the project with useParams
+  const { id } = useParams()
+  // get the status of loading, error, and data from the GET_PROJECT query
+  // Need to pass in an object variables with the id in order to get the data from the query
+  const {loading, error, data } = useQuery(GET_PROJECT, {variables: { id }})
+
+
+  if (loading) return <Spinner />
+  if (error) return <p>Something went wrong</p>
+  
   return (
     <div>Project</div>
   )
