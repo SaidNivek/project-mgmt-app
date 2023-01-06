869c80f73a3559ffa19ead4b0d55d6fb4476e6e0
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-06 18:23:16 -0500

    Remove <Spinner /> from Projects.jsx bc there were 2 spinners on load

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -6,7 +6,7 @@ import ProjectCard from "./ProjectCard"
 export default function Projects() {
     const { loading, error, data } = useQuery(GET_PROJECTS)
 
-    if (loading) return <Spinner />
+    if (loading) return null
     if (error) return <p>Something went wrong</p>
 
     return (
