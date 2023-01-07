8dc1002768ac0f59b7e59b5d48c03b0871eb7a51
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-07 10:16:59 -0500

    Create DELETE_PROJECT in project mutations

--- client/src/components/DeleteProjectButton.jsx
+++ client/src/components/DeleteProjectButton.jsx
@@ -1,3 +1,8 @@
+import { useNavigate } from "react-router-dom"
+import { FaTrash } from "react-icons/fa"
+import { GET_PROJECTS } from "../queries/projectQueries"
+import { useMutation } from "@apollo/client"
+
 export default function DeleteProjectButton() {
   return (
     <div>DeleteProjectButton</div>
